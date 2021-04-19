659a15b7b51548eb3a1aa59eecd929c9d366822c
chonasan <tk24120824@gmail.com>
2021-04-19 16:19:50 +0000

    [update]シフト作成機能

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -1,13 +1,39 @@
 class Admin::TasksController < ApplicationController
+
   def index
+    @task = Task.new
+    @tasks = Task.all
   end
 
   def create
+    task = Task.new(task_params)
+    if task.save
+      flash[:notice] = "新規ジャンルを追加しました。"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = "ジャンルが入力されていません。"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "ジャンルを編集しました。"
+      redirect_to admin_tasks_path
+    else
+      flash[:alert] = "ジャンルが入力されていません。"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:work_name)
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -23,7 +23,7 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    mypage_path
+    shifts_sent_shift_path
   end
 
   def after_sign_out_path_for(resource)
