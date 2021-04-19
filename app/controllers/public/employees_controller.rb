3b628f949299c37399b742ca2d6ff8ee5d351465
chonasan <tk24120824@gmail.com>
2021-04-19 09:32:55 +0000

    [update]マイページ機能

--- app/controllers/public/employees_controller.rb
+++ app/controllers/public/employees_controller.rb
@@ -1,8 +1,8 @@
 class Public::EmployeesController < ApplicationController
-  
+
   def show
   end
-  
+
   def mypage
     @employee = current_employee
   end
@@ -12,9 +12,25 @@ class Public::EmployeesController < ApplicationController
   end
 
   def update
+     @employee = current_employee
+     if @employee.update(employee_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to mypage_path
+     else
+       render action: :edit
+     end
   end
 
   def index
     @employees = Employee.all
   end
+
+
+   private
+
+   def employee_params
+     params.require(:employee).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+   end
+
+
 end
