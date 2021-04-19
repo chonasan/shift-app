07ce135a022561e388f2f1cfc91505a97b0e8d68
chonasan <tk24120824@gmail.com>
2021-04-19 14:48:38 +0000

    [update]シフト作成機能

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -1,16 +1,14 @@
 class Admin::ShiftsController < ApplicationController
   def index
     @shift = Shift.all
-
     @shift.each do|shift|
       if shift.state_status == 0
-      shift.confirmation_start_time = shift.start_time
-      shift.confirmation_end_time = shift.end_time
-      shift.update(state_status: "1")
-      shift.save
+        shift.confirmation_start_time = shift.start_time
+        shift.confirmation_end_time = shift.end_time
+        shift.update(state_status: "1")
+        shift.save
       end
     end
-
     days = Date.today.wday-1
     if days == -1
       days = 6
@@ -25,7 +23,13 @@ class Admin::ShiftsController < ApplicationController
 
   def destroy
     shift = Shift.find(params[:id])
-    shift.update(state_status: "2")
+    if shift.state_status == 4
+      shift.destroy
+      redirect_to admin_shifts_path
+    else
+     shift.update(state_status: "2")
+     redirect_to admin_shifts_path
+    end
   end
 
   def edit
@@ -41,19 +45,19 @@ class Admin::ShiftsController < ApplicationController
   end
 
   def create
-      year = confirmation_shift_params[:year]
-      month = confirmation_shift_params[:month]
-      date = confirmation_shift_params[:date]
-      start_hour_time = confirmation_shift_params[:start_hour_time]
-      start_minute_time = confirmation_shift_params[:start_minute_time]
-      end_hour_time = confirmation_shift_params[:end_hour_time]
-      end_minute_time = confirmation_shift_params[:end_minute_time]
-      confirmation_start_time = Time.zone.parse("#{year}/#{month}/#{date} #{start_hour_time}:#{start_minute_time}")
-      confirmation_end_time = Time.zone.parse("#{year}/#{month}/#{date} #{end_hour_time}:#{end_minute_time}")
-      shift = Shift.new(confirmation_start_time: confirmation_start_time, confirmation_end_time: confirmation_end_time,employee_id: params[:shift][:employee] )
-      shift.save
-      shift.update(state_status: "4")
-      redirect_to admin_shifts_path, notice: "shift is saved"
+    year = confirmation_shift_params[:year]
+    month = confirmation_shift_params[:month]
+    date = confirmation_shift_params[:date]
+    start_hour_time = confirmation_shift_params[:start_hour_time]
+    start_minute_time = confirmation_shift_params[:start_minute_time]
+    end_hour_time = confirmation_shift_params[:end_hour_time]
+    end_minute_time = confirmation_shift_params[:end_minute_time]
+    confirmation_start_time = Time.zone.parse("#{year}/#{month}/#{date} #{start_hour_time}:#{start_minute_time}")
+    confirmation_end_time = Time.zone.parse("#{year}/#{month}/#{date} #{end_hour_time}:#{end_minute_time}")
+    shift = Shift.new(confirmation_start_time: confirmation_start_time, confirmation_end_time: confirmation_end_time,employee_id: params[:shift][:employee] )
+    shift.save
+    shift.update(state_status: "4")
+    redirect_to admin_shifts_path, notice: "shift is saved"
   end
 
   def show
--- app/controllers/public/shifts_controller.rb
+++ app/controllers/public/shifts_controller.rb
@@ -33,32 +33,18 @@ class Public::ShiftsController < ApplicationController
   end
 
   def create
-    # is_succeeded = true
-    # # start_date = Date.parse("2021-04-04")
-    # # end_date = Date.parse("2021-04-10")
-    # # (start_date..end_date).each do |date|
-      year = sent_shift_params[:year]
-      month = sent_shift_params[:month]
-      date = sent_shift_params[:date]
-      start_hour_time = sent_shift_params[:start_hour_time]
-      start_minute_time = sent_shift_params[:start_minute_time]
-      end_hour_time = sent_shift_params[:end_hour_time]
-      end_minute_time = sent_shift_params[:end_minute_time]
-      start_time = Time.zone.parse("#{year}/#{month}/#{date} #{start_hour_time}:#{start_minute_time}")
-      end_time = Time.zone.parse("#{year}/#{month}/#{date} #{end_hour_time}:#{end_minute_time}")
-      shift = current_employee.shifts.new(start_time: start_time, end_time: end_time)
-      shift.save
-      redirect_to shifts_sent_shift_path, notice: "shift is saved"
-    #   unless shift.save
-    #     is_succeeded = false
-    #   end
-    # end
-
-    # if is_succeeded
-    #   redirect_to shifts_path, notice: "shift is saved"
-    # else
-    #   render :sent_shift
-    # end
+    year = sent_shift_params[:year]
+    month = sent_shift_params[:month]
+    date = sent_shift_params[:date]
+    start_hour_time = sent_shift_params[:start_hour_time]
+    start_minute_time = sent_shift_params[:start_minute_time]
+    end_hour_time = sent_shift_params[:end_hour_time]
+    end_minute_time = sent_shift_params[:end_minute_time]
+    start_time = Time.zone.parse("#{year}/#{month}/#{date} #{start_hour_time}:#{start_minute_time}")
+    end_time = Time.zone.parse("#{year}/#{month}/#{date} #{end_hour_time}:#{end_minute_time}")
+    shift = current_employee.shifts.new(start_time: start_time, end_time: end_time)
+    shift.save
+    redirect_to shifts_sent_shift_path, notice: "shift is saved"
   end
 
   private
