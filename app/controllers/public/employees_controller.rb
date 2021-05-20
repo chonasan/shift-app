e6d390ef0b1f470b39cefc2d4954b01172ac376d
chonasan <tk24120824@gmail.com>
2021-05-20 09:36:13 +0000

    [update]contoroller

--- app/controllers/public/employees_controller.rb
+++ app/controllers/public/employees_controller.rb
@@ -54,7 +54,16 @@ class Public::EmployeesController < ApplicationController
    private
 
    def employee_params
-     params.require(:employee).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+     params.require(:employee).permit(
+       :last_name, 
+       :first_name, 
+       :last_name_kana, 
+       :first_name_kana, 
+       :email, 
+       :postal_code, 
+       :address, 
+       :telephone_number, 
+       :is_deleted)
    end
 
 
